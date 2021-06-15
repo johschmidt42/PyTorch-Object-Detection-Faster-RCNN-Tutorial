793fa82172d13a94759a354bf530955db7c4f2ac
johschmidt42 <johannes.schmidt.vik@gmail.com>
2021-06-15 16:29:05 +0200

    added __main__

--- pores/training_scripts/training_script.py
+++ pores/training_scripts/training_script.py
@@ -49,149 +49,155 @@ params = {'BATCH_SIZE': 4,
           'UPLOAD_MODEL': False
           }
 
-# api key
-assert os.environ['NEPTUNE'], 'Did you set your env var NEPTUNE?'
-api_key = os.environ['NEPTUNE']
 
-# root directory
-root = pathlib.Path(r'pores/training_data')
+def main():
+    # api key
+    assert os.environ['NEPTUNE'], 'Did you set your env var NEPTUNE?'
+    api_key = os.environ['NEPTUNE']
 
-# input and target files
-inputs = get_filenames_of_path(root / 'input_224x224')
-targets = get_filenames_of_path(root / 'target_224x224')
+    # root directory
+    root = pathlib.Path(r'../training_data')
 
-assert len(inputs) > 0, f'No files found in: {root / "input_224x224"}'
-assert len(targets) > 0, f'No files found in: {root / "target_224x224"}'
+    # input and target files
+    inputs = get_filenames_of_path(root / 'input_224x224')
+    targets = get_filenames_of_path(root / 'target_224x224')
 
-inputs.sort()
-targets.sort()
+    assert len(inputs) > 0, f'No files found in: {root / "input_224x224"}'
+    assert len(targets) > 0, f'No files found in: {root / "target_224x224"}'
 
-# training transformations and augmentations
-transforms_training = ComposeDouble([
-    Clip(),
-    AlbumentationWrapper(albumentation=A.HorizontalFlip(p=0.5)),
-    AlbumentationWrapper(albumentation=A.VerticalFlip(p=0.5)),
-    FunctionWrapperDouble(np.moveaxis, source=-1, destination=0),
-    FunctionWrapperDouble(normalize_01)
-])
+    inputs.sort()
+    targets.sort()
 
-# validation transformations
-transforms_validation_test = ComposeDouble([
-    Clip(),
-    FunctionWrapperDouble(np.moveaxis, source=-1, destination=0),
-    FunctionWrapperDouble(normalize_01)
-])
+    # training transformations and augmentations
+    transforms_training = ComposeDouble([
+        Clip(),
+        AlbumentationWrapper(albumentation=A.HorizontalFlip(p=0.5)),
+        AlbumentationWrapper(albumentation=A.VerticalFlip(p=0.5)),
+        FunctionWrapperDouble(np.moveaxis, source=-1, destination=0),
+        FunctionWrapperDouble(normalize_01)
+    ])
 
-# random seed
-seed_everything(params['SEED'])
+    # validation transformations
+    transforms_validation_test = ComposeDouble([
+        Clip(),
+        FunctionWrapperDouble(np.moveaxis, source=-1, destination=0),
+        FunctionWrapperDouble(normalize_01)
+    ])
 
-# training validation test split
-inputs_train, inputs_valid, inputs_test = inputs[:16], inputs[16:20], inputs[20:]
-targets_train, targets_valid, targets_test = targets[:16], targets[16:20], targets[20:24]
+    # random seed
+    seed_everything(params['SEED'])
 
-# dataset training
-dataset_train = ObjectDetectionDataSet(inputs=inputs_train,
-                                       targets=targets_train,
-                                       transform=transforms_training,
-                                       use_cache=True,
-                                       convert_to_format=None,
-                                       mapping=None)
+    # training validation test split
+    inputs_train, inputs_valid, inputs_test = inputs[:16], inputs[16:20], inputs[20:]
+    targets_train, targets_valid, targets_test = targets[:16], targets[16:20], targets[20:24]
 
-# dataset validation
-dataset_valid = ObjectDetectionDataSet(inputs=inputs_valid,
-                                       targets=targets_valid,
-                                       transform=transforms_validation_test,
-                                       use_cache=True,
-                                       convert_to_format=None,
-                                       mapping=None)
+    # dataset training
+    dataset_train = ObjectDetectionDataSet(inputs=inputs_train,
+                                           targets=targets_train,
+                                           transform=transforms_training,
+                                           use_cache=True,
+                                           convert_to_format=None,
+                                           mapping=None)
 
-# dataset test
-dataset_test = ObjectDetectionDataSet(inputs=inputs_test,
-                                      targets=targets_test,
-                                      transform=transforms_validation_test,
-                                      use_cache=True,
-                                      convert_to_format=None,
-                                      mapping=None)
+    # dataset validation
+    dataset_valid = ObjectDetectionDataSet(inputs=inputs_valid,
+                                           targets=targets_valid,
+                                           transform=transforms_validation_test,
+                                           use_cache=True,
+                                           convert_to_format=None,
+                                           mapping=None)
 
-# dataloader training
-dataloader_train = DataLoader(dataset=dataset_train,
-                              batch_size=params['BATCH_SIZE'],
-                              shuffle=True,
-                              num_workers=0,
-                              collate_fn=collate_double)
+    # dataset test
+    dataset_test = ObjectDetectionDataSet(inputs=inputs_test,
+                                          targets=targets_test,
+                                          transform=transforms_validation_test,
+                                          use_cache=True,
+                                          convert_to_format=None,
+                                          mapping=None)
 
-# dataloader validation
-dataloader_valid = DataLoader(dataset=dataset_valid,
-                              batch_size=1,
-                              shuffle=False,
-                              num_workers=0,
-                              collate_fn=collate_double)
+    # dataloader training
+    dataloader_train = DataLoader(dataset=dataset_train,
+                                  batch_size=params['BATCH_SIZE'],
+                                  shuffle=True,
+                                  num_workers=0,
+                                  collate_fn=collate_double)
 
-# dataloader test
-dataloader_test = DataLoader(dataset=dataset_test,
-                             batch_size=1,
-                             shuffle=False,
-                             num_workers=0,
-                             collate_fn=collate_double)
+    # dataloader validation
+    dataloader_valid = DataLoader(dataset=dataset_valid,
+                                  batch_size=1,
+                                  shuffle=False,
+                                  num_workers=0,
+                                  collate_fn=collate_double)
 
-# neptune logger
-neptune_logger = NeptuneLogger(
-    api_key=api_key,
-    project_name=f'{params["OWNER"]}/{params["PROJECT"]}',
-    experiment_name=params['EXPERIMENT'],
-    params=params
-)
+    # dataloader test
+    dataloader_test = DataLoader(dataset=dataset_test,
+                                 batch_size=1,
+                                 shuffle=False,
+                                 num_workers=0,
+                                 collate_fn=collate_double)
 
-assert neptune_logger.name  # http GET request to check if the project exists
+    # neptune logger
+    neptune_logger = NeptuneLogger(
+        api_key=api_key,
+        project_name=f'{params["OWNER"]}/{params["PROJECT"]}',
+        experiment_name=params['EXPERIMENT'],
+        params=params
+    )
 
-# model init
-model = get_fasterRCNN_resnet(num_classes=params['CLASSES'],
-                              backbone_name=params['BACKBONE'],
-                              anchor_size=params['ANCHOR_SIZE'],
-                              aspect_ratios=params['ASPECT_RATIOS'],
-                              fpn=params['FPN'],
-                              min_size=params['MIN_SIZE'],
-                              max_size=params['MAX_SIZE'])
+    assert neptune_logger.name  # http GET request to check if the project exists
 
-# task init
-task = FasterRCNN_lightning(model=model, lr=params['LR'], iou_threshold=params['IOU_THRESHOLD'])
+    # model init
+    model = get_fasterRCNN_resnet(num_classes=params['CLASSES'],
+                                  backbone_name=params['BACKBONE'],
+                                  anchor_size=params['ANCHOR_SIZE'],
+                                  aspect_ratios=params['ASPECT_RATIOS'],
+                                  fpn=params['FPN'],
+                                  min_size=params['MIN_SIZE'],
+                                  max_size=params['MAX_SIZE'])
 
-# callbacks
-checkpoint_callback = ModelCheckpoint(monitor='Validation_mAP', mode='max')
-learningrate_callback = LearningRateMonitor(logging_interval='step', log_momentum=False)
-early_stopping_callback = EarlyStopping(monitor='Validation_mAP', patience=params['PATIENCE'], mode='max')
+    # task init
+    task = FasterRCNN_lightning(model=model, lr=params['LR'], iou_threshold=params['IOU_THRESHOLD'])
 
-# trainer init
-trainer = Trainer(gpus=params['GPU'],
-                  precision=params['PRECISION'],  # try 16 with enable_pl_optimizer=False
-                  callbacks=[checkpoint_callback, learningrate_callback, early_stopping_callback],
-                  default_root_dir=params['DEFAULT_ROOT_DIR'],  # where checkpoints are saved to
-                  logger=neptune_logger,
-                  log_every_n_steps=1,
-                  num_sanity_val_steps=0,
-                  benchmark=True,  # good if the input sizes do not change, will increase speed
-                  )
+    # callbacks
+    checkpoint_callback = ModelCheckpoint(monitor='Validation_mAP', mode='max')
+    learningrate_callback = LearningRateMonitor(logging_interval='step', log_momentum=False)
+    early_stopping_callback = EarlyStopping(monitor='Validation_mAP', patience=params['PATIENCE'], mode='max')
 
-# start training
-trainer.max_epochs = params['MAXEPOCHS']
-trainer.fit(task,
-            train_dataloader=dataloader_train,
-            val_dataloaders=dataloader_valid)
+    # trainer init
+    trainer = Trainer(gpus=params['GPU'],
+                      precision=params['PRECISION'],  # try 16 with enable_pl_optimizer=False
+                      callbacks=[checkpoint_callback, learningrate_callback, early_stopping_callback],
+                      default_root_dir=params['DEFAULT_ROOT_DIR'],  # where checkpoints are saved to
+                      logger=neptune_logger,
+                      log_every_n_steps=1,
+                      num_sanity_val_steps=0,
+                      benchmark=True,  # good if the input sizes do not change, will increase speed
+                      )
 
-# start testing
-trainer.test(ckpt_path='best', test_dataloaders=dataloader_test)
+    # start training
+    trainer.max_epochs = params['MAXEPOCHS']
+    trainer.fit(task,
+                train_dataloader=dataloader_train,
+                val_dataloaders=dataloader_valid)
 
-# log packages
-log_packages_neptune(neptune_logger)
+    # start testing
+    trainer.test(ckpt_path='best', test_dataloaders=dataloader_test)
 
-# log model
-if params['UPLOAD_MODEL']:
-    checkpoint_path = pathlib.Path(checkpoint_callback.best_model_path)
-    log_model_neptune(checkpoint_path=checkpoint_path,
-                      save_directory=pathlib.Path.home(),
-                      name='best_model.pt',
-                      neptune_logger=neptune_logger)
+    # log packages
+    log_packages_neptune(neptune_logger)
 
-# stop logger
-neptune_logger.experiment.stop()
-print('Finished')
+    # log model
+    if params['UPLOAD_MODEL']:
+        checkpoint_path = pathlib.Path(checkpoint_callback.best_model_path)
+        log_model_neptune(checkpoint_path=checkpoint_path,
+                          save_directory=pathlib.Path.home(),
+                          name='best_model.pt',
+                          neptune_logger=neptune_logger)
+
+    # stop logger
+    neptune_logger.experiment.stop()
+    print('Finished')
+
+
+if __name__ == '__main__':
+    main()
