39e2856bdd8c90a61a58e0d521270a30c87a4e28
johschmidt42 <johannes.schmidt.vik@gmail.com>
2021-06-16 10:11:09 +0200

    moved and clean up

--- pytorch_faster_rcnn_tutorial/metrics/pascal_voc_evaluator_test.py
+++ pytorch_faster_rcnn_tutorial/metrics/pascal_voc_evaluator_test.py
@@ -1,15 +1,16 @@
-# %% Imports
+# Imports
 import pathlib
-from utils import get_filenames_of_path
-from utils import from_file_to_BoundingBox
 from itertools import chain
-from metrics.pascal_voc_evaluator import get_pascalvoc_metrics
-from metrics.enumerators import MethodAveragePrecision
 
-# %% root directory
+from pytorch_faster_rcnn_tutorial.metrics.enumerators import MethodAveragePrecision
+from pytorch_faster_rcnn_tutorial.metrics.pascal_voc_evaluator import get_pascalvoc_metrics
+from pytorch_faster_rcnn_tutorial.utils import from_file_to_boundingbox
+from pytorch_faster_rcnn_tutorial.utils import get_filenames_of_path
+
+# root directory
 root = pathlib.Path(r"C:\Users\johan\Desktop\Johannes\Heads")
 
-# %% input and target files
+# input and target files
 inputs = get_filenames_of_path(root / 'input')
 targets = get_filenames_of_path(root / 'target')
 
@@ -17,11 +18,11 @@ inputs.sort()
 targets.sort()
 
 # get the gt_boxes from disk
-gt_boxes = [from_file_to_BoundingBox(file_name, groundtruth=True) for file_name in targets]
+gt_boxes = [from_file_to_boundingbox(file_name, groundtruth=True) for file_name in targets]
 # reduce list
 gt_boxes = list(chain(*gt_boxes))
 # TODO: add predictions
-pred_boxes = [from_file_to_BoundingBox(file_name, groundtruth=False) for file_name in targets]
+pred_boxes = [from_file_to_boundingbox(file_name, groundtruth=False) for file_name in targets]
 pred_boxes = list(chain(*pred_boxes))
 
 output = get_pascalvoc_metrics(gt_boxes=gt_boxes,
